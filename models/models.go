1dc0d13b7d1fd7d9f4c321b1dc2ded25dd388d4e
ankit.kumar-at-527579388740 <ankit.kumar@borderfree.io>
2023-10-04 20:23:48 +0530

    added-pagination

--- models/models.go
+++ models/models.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"my-ankit-practice/controller"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var collection *mongo.Collection
+var mu sync.Mutex
 
 func init() {
 	collection = controller.ConnectionDbInstance()
@@ -27,18 +31,40 @@ type Blog struct {
 }
 
 func GetBlog(c *gin.Context) {
+	pageStr := c.DefaultQuery("page", "1")
+	limitStr := c.DefaultQuery("limit", "10")
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid page parameter"})
+		return
+	}
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid limit parameter"})
+		return
+	}
+	skip := (page - 1) * limit
+	mu.Lock()
+	defer mu.Unlock()
 	var data []Blog
-	cursor, _ := collection.Find(context.Background(), bson.M{})
+	cursor, err := collection.Find(context.Background(), bson.M{}, options.Find().SetLimit(int64(limit)).SetSkip(int64(skip)))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch data"})
+		return
+	}
 	defer cursor.Close(context.Background())
 	for cursor.Next(context.Background()) {
 		var blog Blog
 		cursor.Decode(context.Background())
 		data = append(data, blog)
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": data})
+	c.JSON(http.StatusOK, gin.H{"message": data})
 }
 
 func CraetePost(c *gin.Context) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	var blog Blog
 	err := c.ShouldBindJSON(&blog)
 	if err != nil {
@@ -56,6 +82,8 @@ func CraetePost(c *gin.Context) {
 }
 
 func UpdatePost(c *gin.Context) {
+	mu.Lock()
+	defer mu.Unlock()
 	id := c.Param("id")
 	var post Blog
 	if err := c.ShouldBindJSON(&post); err != nil {
@@ -68,18 +96,22 @@ func UpdatePost(c *gin.Context) {
 	rst, err := collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": rst})
-
 }
 
 func DelectePost(c *gin.Context) {
+	mu.Lock()
+	defer mu.Unlock()
+
 	id := c.Param("id")
 	ID, _ := primitive.ObjectIDFromHex(id)
 	filter := bson.M{"_id": ID}
 	_, err := collection.DeleteOne(context.Background(), filter)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Blog post deleted successfully"})
 }
